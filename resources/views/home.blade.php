855c8589e54093159d698f46455fb14e50493695
hamroz <hamroz.gavharov@gmail.com>
2022-06-03 03:46:08 +0600

    minor changes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,17 +3,17 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <div class="col-3 p-5">
-                <img class="rounded-circle " style="max-height:200px" src="/svg/rndm.jpg" alt="">
+            <div class="col-3 p-3">
+                <img class="rounded-circle" style="max-height:150px" src="/svg/rndm.jpg" alt="">
             </div>
-            <div class="col-9">
+            <div class="col-9 pt-3">
                 <div>
                     <h1>laravel</h1>
                 </div>
-                <div>
-                    <div><strong>153</strong> posts</div>
-                    <div><strong>23k</strong> followers</div>
-                    <div><strong>212</strong> following</div>
+                <div class="d-flex">
+                    <div class="p-1"><strong>153</strong> posts</div>
+                    <div class="p-1"><strong>23k</strong> followers</div>
+                    <div class="p-1"><strong>212</strong> following</div>
                 </div>
             </div>
         </div>
